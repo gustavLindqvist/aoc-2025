c75c71ac19501d5bda5cca6ca47be7f9b8a37c90
gustavLindqvist <gustav.lindqvist@ingka.com>
2025-12-01 21:43:35 +0100

    Day1

--- internal/days/day01/star1.go
+++ internal/days/day01/star1.go
@@ -1,6 +1,29 @@
 package day01
 
+import (
+	"strconv"
+
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
 func Star1(input string) any {
-	// TODO: Implement star 1
-	return nil
+	pos := 50
+	count := 0
+
+	for _, line := range aoc.Lines(input) {
+		dir := line[0]
+		steps, _ := strconv.Atoi(line[1:])
+
+		switch dir {
+		case 'L':
+			pos = ((pos-steps)%100 + 100) % 100
+		case 'R':
+			pos = ((pos+steps)%100 + 100) % 100
+		}
+		if pos == 0 {
+			count += 1
+		}
+	}
+
+	return count
 }
--- internal/days/day01/star2.go
+++ internal/days/day01/star2.go
@@ -1,6 +1,41 @@
 package day01
 
+import (
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+	"strconv"
+)
+
 func Star2(input string) any {
-	// TODO: Implement star 2
-	return nil
+	pos := 50
+	count := 0
+
+	for _, line := range aoc.Lines(input) {
+		dir := line[0]
+		steps, _ := strconv.Atoi(line[1:])
+		lo, hi := 0, 0
+
+		switch dir {
+		case 'L':
+			lo = pos - steps
+			hi = pos - 1
+			pos = ((pos-steps)%100 + 100) % 100
+		case 'R':
+			lo = pos + 1
+			hi = pos + steps
+			pos = ((pos+steps)%100 + 100) % 100
+		}
+
+		if lo <= hi {
+			count += divDown(hi) - divDown(lo-1)
+		}
+	}
+
+	return count
+}
+
+func divDown(a int) int {
+	if a >= 0 {
+		return a / 100
+	}
+	return (a - 99) / 100
 }
