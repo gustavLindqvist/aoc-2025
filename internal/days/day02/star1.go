42ea9a227bffb6da84b6fde98382d3ed409f915f
gustavLindqvist <gustav.lindqvist@ingka.com>
2025-12-02 16:25:58 +0100

    Day 02

--- internal/days/day02/star1.go
+++ internal/days/day02/star1.go
@@ -1,6 +1,50 @@
 package day02
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Star1(input string) any {
-	// TODO: Implement star 1
-	return nil
+	var lines = strings.Split(input, ",")
+	var count = 0
+	for _, set := range lines {
+		s := strings.Split(set, "-")
+		var start, _ = strconv.Atoi(s[0])
+		var end, _ = strconv.Atoi(s[1])
+		for i := start; i <= end; i++ {
+			if repeating(i) {
+				count += i
+			}
+		}
+	}
+	return count
+}
+
+func repeating(num int) bool {
+	var len = digits(num)
+	if len%2 == 0 {
+		var pow = 1
+		for range len / 2 {
+			pow *= 10
+		}
+		var first = num % pow
+		for i := num / pow; i > 0; i = i / pow {
+			if first != i%pow {
+				return false
+			}
+		}
+		return true
+	}
+
+	return false
+}
+
+func digits(i int) int {
+	x, count := 10, 1
+	for x <= i {
+		x *= 10
+		count++
+	}
+	return count
 }
--- internal/days/day02/star2.go
+++ internal/days/day02/star2.go
@@ -1,6 +1,45 @@
 package day02
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Star2(input string) any {
-	// TODO: Implement star 2
-	return nil
+	var lines = strings.Split(input, ",")
+	var count = 0
+	for _, set := range lines {
+		s := strings.Split(set, "-")
+		var start, _ = strconv.Atoi(s[0])
+		var end, _ = strconv.Atoi(s[1])
+		for i := start; i <= end; i++ {
+			if repeating2(i) {
+				count += i
+			}
+		}
+	}
+	return count
+}
+
+func repeating2(num int) bool {
+	var len = digits(num)
+	for times := 2; times <= len; times++ {
+		if len%times == 0 {
+			var pow = 1
+			for range len / times {
+				pow *= 10
+			}
+			var first = num % pow
+			var fail = false
+			for i := num / pow; i > 0; i = i / pow {
+				if first != i%pow {
+					fail = true
+				}
+			}
+			if !fail {
+				return true
+			}
+		}
+	}
+	return false
 }
