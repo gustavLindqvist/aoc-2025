ef387f9e6c38ab64bec45e30d1bc584259b3c31d
gustavLindqvist <gustav.lindqvist@ingka.com>
2025-12-03 17:12:40 +0100

    Day 03

--- internal/days/day03/star1.go
+++ internal/days/day03/star1.go
@@ -1,6 +1,23 @@
 package day03
 
+import (
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
 func Star1(input string) any {
-	// TODO: Implement star 1
-	return nil
+	var count = 0
+	for _, line := range aoc.Lines(input) {
+		var f, s = 0, 0
+		for i, r := range line {
+			var b = int(r - '0')
+			if b > f && i < len(line)-1 {
+				f = b
+				s = 0
+			} else if b > s {
+				s = b
+			}
+		}
+		count += (f * 10) + s
+	}
+	return count
 }
--- internal/days/day03/star2.go
+++ internal/days/day03/star2.go
@@ -1,6 +1,30 @@
 package day03
 
+import (
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
 func Star2(input string) any {
-	// TODO: Implement star 2
-	return nil
+	var count = 0
+	for _, bank := range aoc.Lines(input) {
+		var bats = make([]int, 12)
+		var m = len(bank) - 1
+		for i, r := range bank {
+			var jolt = int(r - '0')
+			var start = max(0, 11-(m-i))
+			for j := start; j < 12; j++ {
+				if jolt > bats[j] {
+					bats[j] = jolt
+					clear(bats[(j + 1):])
+					break
+				}
+			}
+		}
+		var pow = 1
+		for i := 11; i >= 0; i-- {
+			count += bats[i] * pow
+			pow *= 10
+		}
+	}
+	return count
 }
