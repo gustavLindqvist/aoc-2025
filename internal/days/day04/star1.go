82def4534d3ed8fb79c473ce655587637a4c482d
gustavLindqvist <gustav.lindqvist@ingka.com>
2025-12-04 17:30:55 +0100

    Day 04

--- internal/days/day04/star1.go
+++ internal/days/day04/star1.go
@@ -1,6 +1,36 @@
 package day04
 
+import "github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+
 func Star1(input string) any {
-	// TODO: Implement star 1
-	return nil
+	var grid = make([][]bool, 0)
+	for _, line := range aoc.Lines(input) {
+		var g = make([]bool, 0)
+		for _, r := range line {
+			g = append(g, r == '@')
+		}
+		grid = append(grid, g)
+	}
+	var count = 0
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			if grid[y][x] && (neighbours(grid, x, y) < 4) {
+				count += 1
+			}
+		}
+	}
+
+	return count
+}
+
+func neighbours(grid [][]bool, x int, y int) int {
+	var count = 0
+	for yy := max(0, y-1); yy < min(len(grid), y+2); yy++ {
+		for xx := max(0, x-1); xx < min(len(grid[0]), x+2); xx++ {
+			if grid[yy][xx] {
+				count += 1
+			}
+		}
+	}
+	return (count - 1)
 }
--- internal/days/day04/star2.go
+++ internal/days/day04/star2.go
@@ -1,6 +1,41 @@
 package day04
 
+import (
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
 func Star2(input string) any {
-	// TODO: Implement star 2
-	return nil
+	var grid = make([][]bool, 0)
+	for _, line := range aoc.Lines(input) {
+		var g = make([]bool, 0)
+		for _, r := range line {
+			g = append(g, r == '@')
+		}
+		grid = append(grid, g)
+	}
+	var count = 0
+	for true {
+		var grid2 = make([][]bool, len(grid))
+		var cd = 0
+		for y := 0; y < len(grid); y++ {
+			var g = make([]bool, len(grid[0]))
+			for x := 0; x < len(grid[0]); x++ {
+				if grid[y][x] && (neighbours(grid, x, y) < 4) {
+					cd += 1
+					g[x] = false
+				} else {
+					g[x] = grid[y][x]
+				}
+			}
+			grid2[y] = g
+		}
+		if cd == 0 {
+			return count
+		} else {
+			count += cd
+		}
+		grid = grid2
+	}
+
+	return count
 }
