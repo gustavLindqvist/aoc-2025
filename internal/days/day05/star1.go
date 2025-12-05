fc8b2e289c515ac7e779656c61177b2691e91a76
gustavLindqvist <gustav.lindqvist@ingka.com>
2025-12-05 08:35:45 +0100

    Day 05

--- internal/days/day05/star1.go
+++ internal/days/day05/star1.go
@@ -1,6 +1,36 @@
 package day05
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
+type fresh struct {
+	from int
+	to   int
+}
+
 func Star1(input string) any {
-	// TODO: Implement star 1
-	return nil
+	var parts = strings.Split(input, "\n\n")
+	var first, second = parts[0], parts[1]
+	var inv = make([]fresh, 0)
+	for _, line := range aoc.Lines(first) {
+		var tmp = strings.Split(line, "-")
+		var first, _ = strconv.Atoi(tmp[0])
+		var second, _ = strconv.Atoi(tmp[1])
+		inv = append(inv, fresh{first, second})
+	}
+	var count = 0
+	for _, line := range aoc.Lines(second) {
+		var item, _ = strconv.Atoi(line)
+		for _, f := range inv {
+			if item >= f.from && item <= f.to {
+				count += 1
+				break
+			}
+		}
+	}
+	return count
 }
--- internal/days/day05/star2.go
+++ internal/days/day05/star2.go
@@ -1,6 +1,41 @@
 package day05
 
+import (
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/gustavLindqvist/aoc-2025/pkg/aoc"
+)
+
 func Star2(input string) any {
-	// TODO: Implement star 2
-	return nil
+	var parts = strings.Split(input, "\n\n")
+	var first, _ = parts[0], parts[1]
+	var inv = make([]fresh, 0)
+
+	for _, line := range aoc.Lines(first) {
+		var tmp = strings.Split(line, "-")
+		var first, _ = strconv.Atoi(tmp[0])
+		var second, _ = strconv.Atoi(tmp[1])
+		inv = append(inv, fresh{first, second})
+	}
+
+	sort.Slice(inv, func(i, j int) bool {
+		if inv[i].from != inv[j].from {
+			return inv[i].from < inv[j].from
+		}
+		return inv[i].to < inv[j].to
+	})
+	var count = 0
+	var lastEnd = 0
+	for _, f := range inv {
+		if f.from > lastEnd {
+			count += 1
+		}
+		var from = max(lastEnd, f.from)
+		var to = max(lastEnd, f.to)
+		count += to - from
+		lastEnd = to
+	}
+	return count
 }
